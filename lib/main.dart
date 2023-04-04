b91f0eeec18edc3fb8d952486953df77d87ffc3b
Ervanariyulianto <yuliantoervan70@gmail.com>
2023-04-04 10:08:02 +0700

    Menu Pasien dan Pegawai

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/baris_kolom.dart';
-import 'package:flutter_application_1/hello_world.dart';
-import 'baris_kolom.dart';
+import '/ui/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +9,10 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Klinik',
-      home: BarisKolom(),
+    return MaterialApp(
+      title: 'Vann',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
---
+++ lib/model/database.dart
@@ -0,0 +1,64 @@
+List<Map> pegawai = [
+  {
+    'id': 1,
+    'nip': '12345',
+    'nama': 'Albert Iskandar',
+    'tanggalLahir': '15 April 2002',
+    'nomorTelepon': '081215678709',
+    'email': 'Alberteis@gmail.com',
+    'password': '123',
+  },
+  {
+    'id': 2,
+    'nip': '23456',
+    'nama': 'Clayy',
+    'tanggalLahir': ' 20 Agustus 2004',
+    'nomorTelepon': '082345730884',
+    'email': 'Clayy@gmail.com',
+    'password': '123',
+  },
+  {
+    'id': 3,
+    'nip': '34567',
+    'nama': 'Calvin winata',
+    'tanggalLahir': '29 Juli 1994',
+    'nomorTelepon': '081456529933',
+    'email': 'Winata@gmail.com',
+    'password': '123',
+  },
+];
+
+List<Map> pasien = [
+  {
+    'id': 1,
+    'noRm': '45678',
+    'nama': 'Nicky kiboy',
+    'tanggalLahir': '3 Juni 2000',
+    'nomorTelepon': '0896753838930',
+    'alamat': 'Manado',
+  },
+  {
+    'id': 2,
+    'noRm': '56789',
+    'nama': 'Muhammad adi',
+    'tanggalLahir': '28 Februrari 1995',
+    'nomorTelepon': '0821355678499',
+    'alamat': 'Jakarta',
+  },
+  {
+    'id': 3,
+    'noRm': '67890',
+    'nama': 'Lemona',
+    'tanggalLahir': '30 Oktober 2000',
+    'nomorTelepon': '085556345886',
+    'alamat': 'Wonogiri',
+  },
+  {
+    'id': 4,
+    'noRm': '78912',
+    'nama': 'Skylar tendean',
+    'tanggalLahir': '16 Desember 2002',
+    'nomorTelepon': '085789654321',
+    'alamat': 'Manado',
+  },
+];
